c68c3808e4c50d04bfac2cb6a0586f88adb476ac
Sanket Hebbal <sankethebbal100@gmail.com>
2019-11-22 21:05:09 +0530

    server configration

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-//Install express server
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/prodisc'));
+
+// app.get('/*', function(req,res) {
+    
+// res.sendFile(path.join(__dirname,'/dist/prodisc/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+
+
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/prodisc'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/prodisc'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/prodisc/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/prodisc/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+// default Heroku PORT
 app.listen(process.env.PORT || 8080);
